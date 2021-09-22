28f001479cb5597c822a664a119b96af08faae81
sluxh <s195467@student.dtu.dk>
2021-09-22 23:33:24 +0200

    finished header and implemented Arvo font

--- src/Header.js
+++ src/Header.js
@@ -3,9 +3,14 @@ import MenuIcon from '@mui/icons-material/Menu';
 
 export class Header extends React.Component {
     render() {
-        return <div>
-            <MenuIcon />
-            <h1 class = "font-bold text-3xl">ShiftPlanner</h1>
+        return <div class="flex items-center h-20 bg-primary ">
+            <div class="w-10 h-10 mx-2 ">
+                <MenuIcon type="button" class="hover:text-white fill-current text-gray-400"/>
+            </div>
+            <h1 class="font-bold text-5xl text-white ml-2">ShiftPlanner</h1>
+            <div class="container:">
+
+            </div>
         </div>
     }
 }
