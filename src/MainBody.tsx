b1d1064062996a1968f6556e71fffef3f4376344
sluxh <s195467@student.dtu.dk>
2021-10-07 17:16:17 +0200

    Navbar can now collapse with burger menu press

--- src/MainBody.tsx
+++ src/MainBody.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Calendar } from './components/calendar_Components/Calendar_View_Month'
 import { Header } from './Header'
@@ -8,12 +8,14 @@ import { Login } from './loginPage/LoginView'
 import { NavigationBar } from './NavigationBar'
 
 export const MainBody = () => {
+    let [navBarCollapsed, setNavBarCollapsed] = useState(false);
+
     return (
         <BrowserRouter>
             <div className="flex flex-col min-h-screen">
-                <Header />
+                <Header collapsed={navBarCollapsed} burgerMenuOnClick={setNavBarCollapsed}/>
                 <div className="flex flex-row flex-grow">
-                    <NavigationBar />
+                    <NavigationBar collapsed = {navBarCollapsed}/>
                     <div className="flex flex grow w-screen bg-secondary">
                         <Switch>
                             <Route exact path="/" render={() => <Home />} />
--- src/NavigationBar.tsx
+++ src/NavigationBar.tsx
@@ -1,39 +1,71 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import HomeIcon from '@mui/icons-material/Home'
 import AssignmentIcon from '@mui/icons-material/Assignment'
 import BadgeIcon from '@mui/icons-material/Badge'
 import GroupIcon from '@mui/icons-material/Group'
 import { useHistory } from 'react-router-dom'
 
-export const NavigationBar = () => {
-    const history = useHistory()
-    const [navbarOpen, setNavbarOpen] = React.useState(false);
-    return (
-        <div className="flex flex-col w-64 bg-primary text-white text-lg select-none">
-            <ul className="flex flex-col flex-fill px-2 gap-1 inline-block align-top">
-            <button onClick={() => history.push(`/`)}>
-                <li className="flex hover:bg-secondary rounded-sm px-5 py-4 ">
-                    <div className="flex flex-row"><HomeIcon/> <p className="pl-3">Hjem</p> </div>
-                </li>
-            </button>
-            <button onClick={() => history.push(`/calendar`)}>
-                <li className="flex hover:bg-secondary rounded-sm px-5 py-4">
-                    <div className="flex flex-row"><AssignmentIcon/> <p className="pl-3">Vagtplan</p> </div>
-                </li>
-            </button>
-            <button onClick={() => history.push(`/login`)}>
-                <li className="flex hover:bg-secondary rounded-sm px-5 py-4">
-                    <div className="flex flex-row"><BadgeIcon/> <p className="pl-3">Mine vagter</p> </div>
-                </li>
-            </button>
-                <li className="flex hover:bg-secondary rounded-sm px-5 py-4">
-                    <button>
-                        <div className="flex flex-row"><GroupIcon/> <p className="pl-3">Medarbejere</p> </div>
-                    </button>
-                </li>
-            </ul>
-            <div className="flex flex-grow"></div>
 
-        </div>
-    )
+export const NavigationBar = (props) => {
+    const history = useHistory()
+
+    if(!props.collapsed) {
+        return (
+            <div className="flex flex-col w-64 bg-primary text-white text-lg select-none">
+                <ul className="flex flex-col flex-fill px-2 gap-1 inline-block align-top">
+                    <button onClick={() => history.push(`/`)}>
+                        <li className="flex h-16 hover:bg-secondary rounded-sm px-5 py-4 ">
+                            <div className="flex flex-row"><HomeIcon/> <p className="pl-3">Hjem</p> </div>
+                        </li>
+                    </button>
+                    <button onClick={() => history.push(`/calendar`)}>
+                        <li className="flex h-16 hover:bg-secondary rounded-sm px-5 py-4">
+                            <div className="flex flex-row"><AssignmentIcon/> <p className="pl-3">Vagtplan</p> </div>
+                        </li>
+                    </button>
+                    <button onClick={() => history.push(`/login`)}>
+                        <li className="flex h-16 hover:bg-secondary rounded-sm px-5 py-4">
+                            <div className="flex flex-row"><BadgeIcon/> <p className="pl-3">Mine vagter</p> </div>
+                        </li>
+                    </button>
+                        <li className="flex h-16 hover:bg-secondary rounded-sm px-5 py-4">
+                        <button>
+                            <div className="flex flex-row"><GroupIcon/> <p className="pl-3">Medarbejere</p> </div>
+                        </button>
+                        </li>
+                </ul>
+                <div className="flex flex-grow"></div>
+
+            </div>
+        )
+    } else {
+        return (
+            <div className="flex flex-col w-26 bg-primary text-white text-lg select-none">
+                <ul className="flex flex-col flex-fill px-2 gap-1 inline-block align-top">
+                    <button onClick={() => history.push(`/`)}>
+                        <li className="flex h-16 hover:bg-secondary rounded-sm px-5 py-4 ">
+                            <div className="flex flex-row"><HomeIcon/></div>
+                        </li>
+                    </button>
+                    <button onClick={() => history.push(`/calendar`)}>
+                        <li className="flex h-16 hover:bg-secondary rounded-sm px-5 py-4">
+                            <div className="flex flex-row"><AssignmentIcon/> </div>
+                        </li>
+                    </button>
+                    <button onClick={() => history.push(`/login`)}>
+                        <li className="flex h-16 hover:bg-secondary rounded-sm px-5 py-4">
+                            <div className="flex flex-row"><BadgeIcon/> </div>
+                        </li>
+                    </button>
+                        <li className="flex h-16 hover:bg-secondary rounded-sm px-5 py-4">
+                        <button>
+                            <div className="flex flex-row"><GroupIcon/></div>
+                        </button>
+                        </li>
+                </ul>
+                <div className="flex flex-grow"></div>
+
+            </div>
+        )
+    }
 }
