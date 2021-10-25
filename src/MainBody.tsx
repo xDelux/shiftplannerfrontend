c674cade42dabd2b63cff748e999f16be403b5fc
lillebrian <s195456@student.dtu.dk>
2021-10-25 08:18:43 +0200

    Day view yoink forsøg 30024

--- src/MainBody.tsx
+++ src/MainBody.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { CalenderDay} from './components/calendar_Components/Calender_View_Day'
 import { Calendar } from './components/calendar_Components/Calendar_View_Month'
 import { Header } from './Header'
 import { Home } from './Home'
@@ -14,11 +15,11 @@ export const MainBody = () => {
                 <Header />
                 <div className="flex flex-row flex-grow">
                     <NavigationBar />
-                    <div className="flex flex grow w-screen bg-secondary">
+                    <div className="flex flex-grow bg-secondary">
                         <Switch>
                             <Route exact path="/" render={() => <Home />} />
                             <Route exact path="/login" render={() => <Login />} />
-                            <Route exact path="/calendar" render={() => <Calendar />} />
+                            <Route exact path="/calendar" render={() => <CalenderDay />} />
                         </Switch>
                     </div>
                 </div>
---
+++ src/components/calendar_Components/Calender_View_Day.tsx
@@ -0,0 +1,101 @@
+import React, {useState, useEffect} from "react";
+import { addMonths, format, isSameMonth, addDays, startOfWeek, endOfWeek, startOfMonth, endOfMonth, getWeek, getISOWeek, isSameDay  } from "date-fns";
+import {ShiftComponent} from "./Calender_Shift_Component";
+
+export const CalenderDay = () => {
+
+    const [currentDay, setCurrentDay] = useState(new Date());
+
+    const [shifts, setShifts] = useState([
+        {"name":"Shania", "timeStart":"08:00", "timeEnd":"16:00", "date":new Date()},
+        {"name":"Mikkel", "timeStart":"10:00", "timeEnd":"14:00", "date":addDays(new Date(), -1)},
+        {"name":"Mads", "timeStart":"??:??", "timeEnd":"??:??", "date":new Date()},
+        {"name":"Jacob", "timeStart":"09:00", "timeEnd":"16:00", "date":addDays(new Date(), -3)}])
+
+
+    function nextDay() {
+        setCurrentDay(addDays(currentDay, +1))
+    }
+    function prevDay() {
+        setCurrentDay(addDays(currentDay, -1))
+    }
+
+    const renderHeader = () => {
+        const dateFormat = "B..BBB";
+        return (
+            <div className="header">
+                <div className="icon cursor-pointer ml-5" onClick={prevDay}> chevron_left </div>
+                <span className=""> {format(currentDay, dateFormat)} </span>
+                <div className="icon cursor-pointer" onClick={nextDay}> chevron_right </div>
+            </div>
+        );
+    }
+
+
+    function renderCells() {
+        let columns = [] as any;
+
+
+        for (let i = 1; i <= 24; i++) {
+            columns.push(
+                <div className="span-1 text-black font-bold flex-grow p-5 text-center"> {i}:00 </div>
+            )
+        }
+
+
+        return (
+            <div className="mx-5 mt-5">
+                <div
+                    className="flex items-center flex-row flex-nowrap border-2 border-black divide-x-2 divide-black bg-darkgrey">
+                    {columns}
+                </div>
+                <div className="relative flex flex-col border-2 border-black divide-y divide-black bg-lightgrey">
+                    <div className="">
+                        <div className=" text-white font-bold col-span-5"> 00</div>
+                    </div>
+
+                    <div className="left-10 text-white font-bold"> 00</div>
+
+                </div>
+            </div>
+
+        )
+    }
+        function renderGrid() {
+
+            let columns = [] as any;
+
+
+            for (let i = 1; i <= 24; i++) {
+                columns.push(
+                    <div className="text-black font-bold p-3 text-center"> {i}:00 </div>
+                )
+            }
+
+
+            return (
+                <div className="mx-5 mt-5 flex flex-col-reverse">
+                    <div className="flex flex-row items-center border-2 border-black divide-x-2 divide-black bg-darkgrey order-5">
+                            {columns}
+                    </div>
+                    <div className="grid grid-cols-24 w- divide-y-2 divide-y-2 border-2 border-black bg-lightgrey place-items-center span-1">
+                            <div className="text-white font-bold"> 00</div>
+                            <div className="text-white font-bold"> 00</div>
+                            <div className="text-white font-bold"> 00</div>
+
+                        </div>
+
+                </div>
+
+            )
+        }
+
+    return (
+        <div className="">
+            {/*{renderHeader()}*/}
+            {/*{renderWorkers()}*/}
+            {/*{renderCells()}*/}
+            {renderGrid()}
+        </div>
+    );
+}
